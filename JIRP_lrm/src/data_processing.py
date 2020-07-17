5d69b6e12e96b660918ff3897885eb65713e3bb0
Aditya Ojha <adiojha629@gmail.com>
2020-07-16 19:17:31 -0500

    All trails added, now time to anaylze the data

--- JIRP_lrm/src/data_processing.py
+++ JIRP_lrm/src/data_processing.py
@@ -1,10 +1,36 @@
 #get file
-file = open("../results/LRM/lrm-qrm/trail_5/officeworld/lrm-lrm-qrm-0_rewards_over_time.txt")
-isone = False
+file = open("../results/LRM/lrm-qrm/trail_0/officeworld/lrm-lrm-qrm-0_rewards_over_time.txt")
 for line in file.readlines():
-    num = int(line.replace(" ","").replace("\n","")[-1])
-    if(num == 1):
-        isone = True
-        print(num)
-file.close()
-print(isone)
+    
+    if "18" in line:
+        print(line)
+        break
+"""
+for i in range(10):
+    file = open("../results/LRM/lrm-qrm/trail_"+str(i)+"/officeworld/lrm-lrm-qrm-0_rewards_over_time.txt")
+    isone = False
+    istwo = False
+    for line in file.readlines():
+        num = int(line.replace(" ","").replace("\n","")[-1])
+        if(num == 1):
+            isone = True
+            #print(num)
+        if(num == 8):
+            istwo = True
+            #print(num)
+    file.close()
+    #print(isone)
+    print("for trail " + str(i)+ " Is their an 8: " + str(istwo))
+"""
+"""
+list = []
+for i in range(10):
+    for _ in range(10):
+        list.append(i)
+list_new = []
+last_num = 0
+for num in list:
+    list_new.append(num-last_num)
+    last_num = num
+print(list_new)
+"""
